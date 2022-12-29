22f44527b3a2f30ce3c1dbe3ab76dcdd350ca5d8
uku <uku3lig@gmail.com>
2022-12-30 00:42:08 +0100

    fix sodium mixin in 1.18.2

--- src/main/java/net/uku3lig/fractionalgui/mixin/MixinSodiumGameOptionPages.java
+++ src/main/java/net/uku3lig/fractionalgui/mixin/MixinSodiumGameOptionPages.java
@@ -1,17 +1,18 @@
 package net.uku3lig.fractionalgui.mixin;
 
 import me.jellysquid.mods.sodium.client.gui.SodiumGameOptionPages;
-import net.minecraft.client.util.Window;
+import net.minecraft.client.MinecraftClient;
 import net.uku3lig.fractionalgui.FractionalGui;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = SodiumGameOptionPages.class, remap = false)
 public class MixinSodiumGameOptionPages {
-    @Redirect(method = "lambda$general$9", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;calculateScaleFactor(IZ)I"), require = 0)
-    private static int changeScaleFactor(Window instance, int guiScale, boolean forceUnicodeFont) {
-        return instance.calculateScaleFactor(guiScale, forceUnicodeFont) * FractionalGui.getManager().getConfig().getFactor();
+    @ModifyConstant(method = "lambda$general$9", constant = @Constant(intValue = 4))
+    private static int changeScaleFactor(int constant) {
+        return MinecraftClient.getInstance().getWindow().calculateScaleFactor(0, MinecraftClient.getInstance().forcesUnicodeFont())
+                * FractionalGui.getManager().getConfig().getFactor();
     }
 
     private MixinSodiumGameOptionPages() {}
