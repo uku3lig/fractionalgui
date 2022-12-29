591d1abc1da7a7b54b1d81b5f6302f27c42604a7
uku <uku3lig@gmail.com>
2022-12-30 00:34:34 +0100

    add sodium compatibility

--- src/main/java/net/uku3lig/fractionalgui/mixin/MixinSodiumGameOptionPages.java
+++ src/main/java/net/uku3lig/fractionalgui/mixin/MixinSodiumGameOptionPages.java
@@ -1,18 +1,18 @@
 package net.uku3lig.fractionalgui.mixin;
 
-import net.minecraft.client.option.Option;
+import me.jellysquid.mods.sodium.client.gui.SodiumGameOptionPages;
 import net.minecraft.client.util.Window;
 import net.uku3lig.fractionalgui.FractionalGui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(Option.class)
-public class MixinOption {
-    @Redirect(method = "method_32559", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;calculateScaleFactor(IZ)I"))
+@Mixin(value = SodiumGameOptionPages.class, remap = false)
+public class MixinSodiumGameOptionPages {
+    @Redirect(method = "lambda$general$9", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;calculateScaleFactor(IZ)I"), require = 0)
     private static int changeScaleFactor(Window instance, int guiScale, boolean forceUnicodeFont) {
         return instance.calculateScaleFactor(guiScale, forceUnicodeFont) * FractionalGui.getManager().getConfig().getFactor();
     }
 
-    private MixinOption() {}
+    private MixinSodiumGameOptionPages() {}
 }
