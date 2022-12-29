73c3be2a1a163fd43c408e57d5ddff99d5cefcc1
uku <uku3lig@gmail.com>
2022-12-29 19:31:17 +0100

    initial commit

---
+++ src/main/java/net/uku3lig/fractionalgui/FractionalGui.java
@@ -0,0 +1,10 @@
+package net.uku3lig.fractionalgui;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FractionalGui implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
