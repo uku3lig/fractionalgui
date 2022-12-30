151e61cf9dc52407c4f87d71b9ca883ba8ddac02
uku <uku3lig@gmail.com>
2022-12-30 17:56:23 +0100

    make gui scale actually show as decimal value

--- src/main/java/net/uku3lig/fractionalgui/FractionalGui.java
+++ src/main/java/net/uku3lig/fractionalgui/FractionalGui.java
@@ -5,9 +5,15 @@ import net.fabricmc.api.ModInitializer;
 import net.uku3lig.fractionalgui.config.GuiConfig;
 import net.uku3lig.ukulib.config.ConfigManager;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class FractionalGui implements ModInitializer {
     @Getter
     private static final ConfigManager<GuiConfig> manager = ConfigManager.create(GuiConfig.class, "fractionalgui");
+    public static final DecimalFormat FORMAT = new DecimalFormat("0.##", DecimalFormatSymbols.getInstance(Locale.ROOT));
+
     @Override
     public void onInitialize() {
         // hi :3
